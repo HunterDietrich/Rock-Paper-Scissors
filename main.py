1074eba62b83b88894f248170ea3f06fc3918a6a
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-11 23:58:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Project Planning
+#2 Players
+#1) ask player for their choice
+#chooses either rosk, paper, scissors
+#2) computer randomly makes choice
+#3) printing both inputs
+#4) Outputting/Comparing who won!
+#paper beats rock
+#scissors beats paper
+#rock beats scissors
+
+print("***WELCOME TO ROCK PAPER SCISSORS SHOWDOWN***")
