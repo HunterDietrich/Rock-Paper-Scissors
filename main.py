b195ebc76d0e76ab5c7557ba5b6c9ee4c99e5c6b
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-12 00:12:58 +0000

    added code for players choice

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 #Project Planning
 #2 Players
 #1) ask player for their choice
@@ -10,3 +11,14 @@
 #rock beats scissors
 
 print("***WELCOME TO ROCK PAPER SCISSORS SHOWDOWN***")
+
+choices = ["rock", "paper", "scissors"]
+
+player_choice = input("Choose rock, paper, scissors: \n")
+print(f"Player Choice: {player_choice}")
+
+while player_choice not in choices:
+    player_choice = input("Not Valid, Please choose rock, paper, scissors: \n")
+print(f"Player Choice: {player_choice}")
+    
+    
